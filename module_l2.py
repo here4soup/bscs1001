672c79ba9b94e1d95f25983fad507e8ec4287973
here4soup <330803440+here4soup@users.noreply.github.com>
2026-09-19 19:39:16 +1200

    progress into lesson 2

---
+++ module_l2.py
@@ -0,0 +1,69 @@
+# /// 
+# module name:      1001 lecture
+# desc:             tasks and play for lecture per name, and any exercises
+#
+# version:          v1.0
+# major versions:   2026 09 19    -   V1.0 Initial version
+# dependencies: 
+#   none
+# ///
+
+# =========================================================================================================
+# =================
+# tutorial
+# =================
+
+
+
+# 1. format a derived number
+num = 1 / 3 # set a look up
+print(f"{num:.2f}") # format the output
+print(f"{num:.10f}") 
+
+
+
+# 2. nested conditional statement
+# system: initiate prompt
+number = int(input("Give a number: ")) # task the system to prompt input
+
+# user: whakautu in terminal
+
+# system: process the response
+if number >= 0: # nested conditional statement
+    if number % 2 == 0:
+        print("Number is even")
+    else:
+        print("Number is odd")
+else:
+    print("Number was negative")
+
+    
+
+# =========================================================================================================
+# =================
+# exercise 1
+# =================
+
+print("Hi there!")
+print("Haere mai ki te whare")
+print("Kia pai Te Wiki o te Reo Māori")
+
+#LEARN!
+# SHIFT + ENTER = runs line
+# CTRL + F5 = runs entire code
+
+# =================
+# exercise 2
+# =================
+
+# Fix the code
+print("Simeoni")
+print("Juhani")
+print("Eero")
+print("Lauri")
+print("Aapo")
+print("Tuomas")
+print("Timo")
+
+
+# END
