3b29069afedccf10cacbafd7dbb2f1b8f786e126
here4soup <330803440+here4soup@users.noreply.github.com>
2026-09-19 18:52:44 +1200

    next exercise

---
+++ module_e1.py
@@ -0,0 +1,12 @@
+# /// 
+# module name:      1001 exercise 1
+# desc:             tasks and play for exercise
+#
+# version:          v1.0
+# major versions:   2026 09 19    -   V1.0 Initial version
+# dependencies: 
+#   none
+# ///
+
+print("Hi there!")
+print("Haere mai ki te tari")
